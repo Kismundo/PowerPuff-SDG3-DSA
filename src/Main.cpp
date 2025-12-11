b8c7a294a0c68419ec338ef2f24fbef044ea753b
Kismundo <424001623@ntc.edu.ph>
2025-12-11 11:25:04 +0800

    Implement Welcome function in Main.cpp
    Added Welcome function to display a welcome message.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namepsace std;
 
+void Welcome();
+void MainMenu();
+
+void Welcome(){
+    cout << "=======================================\n";
+    cout << "WELCOME TO HEALTH-TRACKER SYSTEM\n";
+    cout << "=======================================\n";
+}
 
 int main(){
+  Welcome();
+  MainMenu();
+  
   return0;
 }
